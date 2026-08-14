2e010d2d4ce89fe5f1829259b387649476b9d29f
ChaosAttractor <lostattractor@gmail.com>
2026-08-14 09:46:08 +0800

    refactor(http): adopt standard dialer APIs

--- protocol/http/http.go
+++ protocol/http/http.go
@@ -2,18 +2,21 @@ package http
 
 import (
 	"context"
-	"fmt"
+	"net"
 	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/daeuniverse/outbound/dialer"
 	"github.com/daeuniverse/outbound/netproxy"
+	"github.com/daeuniverse/outbound/protocol"
 	tls2 "github.com/daeuniverse/outbound/transport/tls"
+	"github.com/samber/oops"
 )
 
 // HttpProxy is an HTTP/HTTPS proxy.
 type HttpProxy struct {
+	protocol.StatelessDialer
 	https     bool
 	transport bool
 	Addr      string
@@ -22,45 +25,34 @@ type HttpProxy struct {
 	HaveAuth  bool
 	Username  string
 	Password  string
-	dialer    netproxy.Dialer
 }
 
-func NewHTTPProxy(u *url.URL, forward netproxy.Dialer) (netproxy.Dialer, error) {
-	s := new(HttpProxy)
-	s.Addr = u.Host
-	s.Path = u.Path
+func NewHTTPProxy(u *url.URL, option *dialer.ExtraOption, parentDialer netproxy.Dialer) (netproxy.Dialer, error) {
+	s := &HttpProxy{
+		StatelessDialer: protocol.StatelessDialer{
+			ParentDialer: parentDialer,
+		},
+		Addr: u.Host,
+		Path: u.Path,
+		Host: u.Query().Get("host"),
+	}
 	if !strings.HasPrefix(s.Path, "/") {
 		s.Path = "/" + s.Path
 	}
-	s.Host = u.Query().Get("host")
-	s.dialer = forward
+
 	if u.User != nil {
 		s.HaveAuth = true
 		s.Username = u.User.Username()
 		s.Password, _ = u.User.Password()
 	}
+
 	s.transport, _ = strconv.ParseBool(u.Query().Get("transport"))
+
 	if u.Scheme == "https" {
 		s.https = true
-		serverName := u.Query().Get("sni")
-		if serverName == "" {
-			serverName = u.Hostname()
-		}
-
-		tlsImplementation := "tls"
-		if u.Query().Get("tlsImplementation") != "" {
-			tlsImplementation = u.Query().Get("tlsImplementation")
-		}
-		alpn := []string{"h2,http/1.1"}
-		if u.Query().Get("alpn") != "" {
-			alpn = []string{u.Query().Get("alpn")}
-		}
-		u := url.URL{
-			Host: s.Addr,
-			RawQuery: url.Values{
-				"sni":  []string{serverName},
-				"alpn": alpn,
-			}.Encode(),
+		alpn := u.Query().Get("alpn")
+		if alpn == "" {
+			alpn = "h2,http/1.1"
 		}
 		allowInsecure, _ := strconv.ParseBool(u.Query().Get("allowInsecure"))
 		if !allowInsecure {
@@ -72,30 +64,29 @@ func NewHTTPProxy(u *url.URL, forward netproxy.Dialer) (netproxy.Dialer, error) 
 		if !allowInsecure {
 			allowInsecure, _ = strconv.ParseBool(u.Query().Get("skipVerify"))
 		}
+		tlsConfig := tls2.TLSConfig{
+			Host:          u.Host,
+			Alpn:          alpn,
+			Sni:           u.Query().Get("sni"),
+			AllowInsecure: allowInsecure,
+		}
 		var err error
-		s.dialer, _, err = tls2.NewTls(&dialer.ExtraOption{
-			AllowInsecure:     allowInsecure,
-			TlsImplementation: tlsImplementation,
-			UtlsImitate:       u.Query().Get("utlsImitate"),
-		}, s.dialer, u.String())
-		if err != nil {
+		if s.ParentDialer, err = tlsConfig.Dialer(option, s.ParentDialer); err != nil {
 			return nil, err
 		}
 	}
 	return s, nil
 }
 
-func (s *HttpProxy) DialContext(ctx context.Context, network, addr string) (netproxy.Conn, error) {
-	magicNetwork, err := netproxy.ParseMagicNetwork(network)
-	if err != nil {
-		return nil, err
-	}
-	switch magicNetwork.Network {
+func (s *HttpProxy) DialContext(ctx context.Context, network, addr string) (net.Conn, error) {
+	switch network {
 	case "tcp":
-		return NewConn(s.dialer, s, addr, network), nil
-	case "udp":
-		return nil, netproxy.UnsupportedTunnelTypeError
+		return NewConn(s.ParentDialer, s, addr, network), nil
 	default:
-		return nil, fmt.Errorf("%w: %v", netproxy.UnsupportedTunnelTypeError, network)
+		return nil, oops.Errorf("%w: %v", netproxy.UnsupportedTunnelTypeError, network)
 	}
+}
+
+func (s *HttpProxy) ListenPacket(ctx context.Context, network string) (net.PacketConn, error) {
+	return nil, oops.Errorf("%w: %v", netproxy.UnsupportedTunnelTypeError, network)
 }
